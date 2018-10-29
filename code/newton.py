9f410447e040fc677c615a39ea1ec541a115413e
neal-o-r <oriainn@tcd.ie>
2018-10-29 16:41:07 +0000

    altered newton

--- code/newton.py
+++ code/newton.py
@@ -8,10 +8,8 @@ def newton(n, a=2, steps=10):
 def newton_recur(n, a=2, i=0, steps=10):
     if i == steps:
         return a
-    return newton_recur(n, a=(a + n/a)/2, i=i+1)
+    return newton_recur(n, a=(a + n/a)/2, i=i+1, steps=steps)
 
 def newton_f(n, guess=2, step=10):
-    next_step = lambda n, a: (a + n/a)/2
-    next10 = curry(next_step, n)
-    return nth(step, iterate(next10, guess))
-
+    next_step = lambda a: (a + n/a)/2
+    return nth(step, iterate(next_step, guess))
